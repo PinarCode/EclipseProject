033ea4119d8e2139f2ede787c86793a13e7e4699
PinarCode <kayapinar4706@gmail.com>
2021-05-29 16:14:48 +0300

    Homework3

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -0,0 +1,43 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="JobTitles")
+public class JobTitle {
+	@Id
+	@GeneratedValue
+	@Column(name="Id")
+	private int id;
+	
+	@Column(name="Title")
+	private String title;
+	
+	public JobTitle() {}
+	
+	public JobTitle(int id, String title) {
+		super();
+		this.id = id;
+		this.title = title;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
